832deeee5e6ebf7a7feb84dcb95622edffe09dfa
Lucas <lucasboscatti@gmail.com>
2020-05-14 13:12:02 -0300

    My Blog app, first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\lucas\OneDrive\Área de Trabalho\ELT20\programacao\blog\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
